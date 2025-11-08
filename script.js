3d375e856c8f30b309ff9b6d8163f2eedb6f3797
Thiago Amorim <46036562+samorimth@users.noreply.github.com>
2025-11-08 14:46:58 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function clearRanking() {
-    if (confirm("Tem certeza que deseja limpar o Ranking? Esta ação não pode ser desfeita.")) {
-        localStorage.removeItem(RANKING_KEY); // Remove o item do localStorage
-        ranking = []; // Zera a variável local do ranking
-        updateRankingDisplay(); // Atualiza a exibição na tela
-        alert("Ranking limpo com sucesso!");
-    }
-}
